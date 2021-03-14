ee1e10a3c32b349b0fd4f00ab9130e69e5157094
Mohammad mahdi aghakhani <2001aghakhani@gmail.com>
2021-03-14 15:32:19 -0400

    single version 1

---
+++ single/script.js
@@ -0,0 +1,28 @@
+/* 
+ 1 : when mouse move in the slider area the function start
+ 2 : the slider "left" will change by the mouse offsetX
+ 3 : change the beforearea by the mouse offsetX
+ 4 : beforearae "width" & slider "left" will be 0 if mouse offsetX less than 30
+ 4 : beforearae "width" & slider "left" will be 400 if mouse offsetX more than 370
+*/
+
+// 1
+const sliderarae = document.querySelector('.slider-area');
+const sliderline = document.querySelector('.slider');
+const beforearea = document.querySelector('.before-area')
+sliderarae.addEventListener('mousemove',(e)=>{
+    // 2
+    sliderline.style.left = e.layerX + 'px';
+    // 3
+    beforearea.style.width = e.layerX + 'px';
+    // 4
+    if(e.layerX < 30){
+    sliderline.style.left = "0px";
+    beforearea.style.width = '0px';
+    }
+    // 5
+    if(e.layerX > 370){
+        sliderline.style.left = "400px";
+        beforearea.style.width = '400px';
+        }
+})
